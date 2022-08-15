4b6f36d404c9c217e543d8e6519fbcb86d82014e
liam <Liamjmiller.15@gmail.com>
2022-08-15 14:38:41 +0100

    Web First Run

--- invoice-project-2/src/main.js
+++ invoice-project-2/src/main.js
@@ -1,11 +1,23 @@
-'use strict';
+"use strict";
 
-function counter() {
+const countingFunction = function counter() {
   let seconds = 0;
   setInterval(() => {
     seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
+    document.getElementById(
+      "app"
+    ).innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
   }, 1000);
-}
+};
 
-counter();
+const openLoginPage = function LoginOpen() {
+  window.open("");
+};
+
+openLoginPage();
+countingFunction();
+
+var opened = window.open("");
+opened.document.write(
+  "<html><head><title>MyTitle</title></head><body>test</body></html>"
+);
