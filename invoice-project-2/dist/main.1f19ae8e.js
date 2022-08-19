cf61b762b96089159db03de765081e8e7b93ba75
liam <Liamjmiller.15@gmail.com>
2022-08-19 02:13:15 +0100

    some css and Html Changes

--- invoice-project-2/dist/main.1f19ae8e.js
+++ invoice-project-2/dist/main.1f19ae8e.js
@@ -183,16 +183,6 @@ var subjectObjectFunction = window.onload = function () {
     }
   };
 };
-
-var site = {
-  Site: []
-};
-var shift = {
-  Shift: ["day", "night", "double"]
-};
-var pay = {
-  Pay: ["150", "300"]
-};
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +211,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49593" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50289" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
