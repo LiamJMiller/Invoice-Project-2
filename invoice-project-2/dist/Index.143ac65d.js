0888e17365227790b990601b77db4254efb7d4c4
liam <Liamjmiller.15@gmail.com>
2022-09-06 00:28:40 +0100

    extra notes section added

--- invoice-project-2/dist/Index.143ac65d.js
+++ invoice-project-2/dist/Index.143ac65d.js
@@ -140,45 +140,6 @@ var loginForm = function loginForm() {
 };
 
 login.addEventListener("click", loginForm); ////////////////////////////////////////////////////////////////
-// const subjectObject = {
-//   "Front-end": {
-//     HTML: ["Links", "Images", "Tables", "Lists"],
-//     CSS: ["Borders", "Margins", "Backgrounds", "Float"],
-//     JavaScript: ["Variables", "Operators", "Functions", "Conditions"],
-//   },
-//   "Back-end": {
-//     PHP: ["Variables", "Strings", "Arrays"],
-//     SQL: ["SELECT", "UPDATE", "DELETE"],
-//   },
-// };
-// const subjectObjectFunction = (window.onload = function () {
-//   const subjectSel = document.getElementById("subject");
-//   const topicSel = document.getElementById("topic");
-//   const chapterSel = document.getElementById("chapter");
-//   for (const x in subjectObject) {
-//     subjectSel.options[subjectSel.options.length] = new Option(x, x);
-//   }
-//   subjectSel.onchange = function () {
-//     //empty Chapters- and Topics- dropdowns
-//     chapterSel.length = 1;
-//     topicSel.length = 1;
-//     //display correct values
-//     for (let y in subjectObject[this.value]) {
-//       topicSel.options[topicSel.options.length] = new Option(y, y);
-//     }
-//   };
-//   topicSel.onchange = function () {
-//     //empty Chapters dropdown
-//     chapterSel.length = 1;
-//     //display correct values
-//     const z = subjectObject[subjectSel.value][this.value];
-//     for (let i = 0; i < z.length; i++) {
-//       chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
-//     }
-//   };
-// });
-
-var hello = "hello"; ////////////////////////////////////////////////////////////////
 // login stuff again
 // Get the modal
 
@@ -265,7 +226,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65319" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56390" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- invoice-project-2/src/index.js
+++ invoice-project-2/src/index.js
@@ -25,49 +25,6 @@ const loginForm = function loginForm() {
 login.addEventListener("click", loginForm);
 
 ////////////////////////////////////////////////////////////////
-
-// const subjectObject = {
-//   "Front-end": {
-//     HTML: ["Links", "Images", "Tables", "Lists"],
-//     CSS: ["Borders", "Margins", "Backgrounds", "Float"],
-//     JavaScript: ["Variables", "Operators", "Functions", "Conditions"],
-//   },
-//   "Back-end": {
-//     PHP: ["Variables", "Strings", "Arrays"],
-//     SQL: ["SELECT", "UPDATE", "DELETE"],
-//   },
-// };
-
-// const subjectObjectFunction = (window.onload = function () {
-//   const subjectSel = document.getElementById("subject");
-//   const topicSel = document.getElementById("topic");
-//   const chapterSel = document.getElementById("chapter");
-//   for (const x in subjectObject) {
-//     subjectSel.options[subjectSel.options.length] = new Option(x, x);
-//   }
-//   subjectSel.onchange = function () {
-//     //empty Chapters- and Topics- dropdowns
-//     chapterSel.length = 1;
-//     topicSel.length = 1;
-//     //display correct values
-//     for (let y in subjectObject[this.value]) {
-//       topicSel.options[topicSel.options.length] = new Option(y, y);
-//     }
-//   };
-//   topicSel.onchange = function () {
-//     //empty Chapters dropdown
-//     chapterSel.length = 1;
-//     //display correct values
-//     const z = subjectObject[subjectSel.value][this.value];
-//     for (let i = 0; i < z.length; i++) {
-//       chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
-//     }
-//   };
-// });
-
-const hello = "hello";
-
-////////////////////////////////////////////////////////////////
 // login stuff again
 
 // Get the modal
